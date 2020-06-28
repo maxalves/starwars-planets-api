b37ac8053af0a72c5aa0757239e5af293ebbe61a
Maxmiller Alves <max.phazen@gmail.com>
2020-06-28 11:47:18 -0300

    Improve custom error to build errors on controller advice

--- src/main/java/com/challenge/starwars/controllers/PlanetControllerAdvice.java
+++ src/main/java/com/challenge/starwars/controllers/PlanetControllerAdvice.java
@@ -2,6 +2,7 @@ package com.challenge.starwars.controllers;
 
 import com.challenge.starwars.exceptions.PlanetAlreadyExistsException;
 import com.challenge.starwars.models.CustomError;
+import com.challenge.starwars.services.PlanetServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,13 +13,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestControllerAdvice(basePackageClasses = {PlanetController.class})
+@RestControllerAdvice(basePackageClasses = {PlanetController.class, PlanetServiceImpl.class})
 public class PlanetControllerAdvice {
 
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(PlanetAlreadyExistsException.class)
     public CustomError planetAlreadyExistsExceptionHandler(PlanetAlreadyExistsException error) {
-        return new CustomError();
+        return CustomError.builder().message(error.getMessage()).build();
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
--- src/main/java/com/challenge/starwars/models/CustomError.java
+++ src/main/java/com/challenge/starwars/models/CustomError.java
@@ -1,4 +1,14 @@
 package com.challenge.starwars.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CustomError {
+    private String message;
 }
