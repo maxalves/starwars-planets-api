842a06e7d89dfbefb5b5068bdd1af598278915c7
Maxmiller Alves <max.phazen@gmail.com>
2020-06-28 13:42:10 -0300

    Add feign and hystrix to project

---
+++ src/main/java/com/challenge/starwars/StarWarsApplication.java
@@ -0,0 +1,24 @@
+package com.challenge.starwars;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+@EnableFeignClients
+public class StarWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+}
