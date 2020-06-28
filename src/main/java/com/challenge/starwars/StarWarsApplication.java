f9011af2810107d931e1348758cc780d424e13b5
Maxmiller Alves <max.phazen@gmail.com>
2020-06-28 12:52:11 -0300

    Create model mapper bean

--- src/main/java/com/challenge/starwars/StarWarsApplication.java
+++ src/main/java/com/challenge/starwars/StarWarsApplication.java
@@ -1,7 +1,9 @@
 package com.challenge.starwars;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StarWarsApplication {
@@ -10,4 +12,8 @@ public class StarWarsApplication {
 		SpringApplication.run(StarWarsApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
