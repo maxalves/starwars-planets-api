0a63c8d528aa1883bf83274b99237ba36c37e5d1
Maxmiller Alves <max.phazen@gmail.com>
2020-06-28 14:39:58 -0300

    Fix add cache name and add usage to external service

--- src/main/java/com/challenge/starwars/services/PlanetServiceImpl.java
+++ src/main/java/com/challenge/starwars/services/PlanetServiceImpl.java
@@ -1,9 +1,10 @@
 package com.challenge.starwars.services;
 
-import com.challenge.starwars.exceptions.PlanetNotFoundException;
 import com.challenge.starwars.exceptions.PlanetAlreadyExistsException;
+import com.challenge.starwars.exceptions.PlanetNotFoundException;
 import com.challenge.starwars.models.Planet;
 import com.challenge.starwars.repositories.PlanetRepository;
+import com.challenge.starwars.services.external.StarWarsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
@@ -15,24 +16,29 @@ import java.util.Objects;
 
 @Service
 public class PlanetServiceImpl implements PlanetService {
-    private final PlanetRepository planetRepository;
-
     private static final String CACHE_NAME = "planets";
+    private final PlanetRepository planetRepository;
+    private final StarWarsService starWarsService;
 
     @Autowired
-    public PlanetServiceImpl(PlanetRepository planetRepository) {
+    public PlanetServiceImpl(PlanetRepository planetRepository, StarWarsService starWarsService) {
         this.planetRepository = planetRepository;
+        this.starWarsService = starWarsService;
     }
 
+    @Cacheable(cacheNames = CACHE_NAME, key = "#id")
     @Override
     public Planet create(Planet planet) {
         if (Objects.nonNull(planetRepository.findByName(planet.getName())))
             throw new PlanetAlreadyExistsException(planet);
 
-        return planetRepository.save(planet);
+        planet = planetRepository.save(planet);
+        setPlanetFilmApparitionsCountOn(planet);
+
+        return planet;
     }
 
-    @Cacheable(cacheNames = CACHE_NAME, key="#id")
+    @Cacheable(cacheNames = CACHE_NAME, key = "#id")
     @Override
     public Planet findById(String id) {
         var planet = planetRepository.findById(id);
@@ -40,12 +46,19 @@ public class PlanetServiceImpl implements PlanetService {
         if (planet.isEmpty())
             throw new PlanetNotFoundException(id);
 
+        starWarsService.getFilmsCount(planet.get().getName())
+                .ifPresent(apparitions -> planet.get().setFilmApparitionsCount(apparitions));
+
         return planet.get();
     }
 
     @Override
     public Page<Planet> findAll(Pageable page) {
-        return planetRepository.findAll(page);
+        var planets = planetRepository.findAll(page);
+
+        planets.getContent().forEach(this::setPlanetFilmApparitionsCountOn);
+
+        return planets;
     }
 
     @Override
@@ -64,5 +77,10 @@ public class PlanetServiceImpl implements PlanetService {
         planetRepository.deleteById(id);
 
         return planet.get();
+    }
+
+    private void setPlanetFilmApparitionsCountOn(Planet planet) {
+        starWarsService.getFilmsCount(planet.getName())
+                .ifPresent(planet::setFilmApparitionsCount);
     }
 }
